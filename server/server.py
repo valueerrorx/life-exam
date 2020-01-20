dd6d8db2631ea80384c535c6c41061050b87bd13
SmanAT <s.hagmann@bgbaden-frauen.ac.at>
2020-01-20 21:14:22 +0100

    sdf

--- server/server.py
+++ server/server.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2017 Thomas Michael Weissel
 #
 # This software may be modified and distributed under the terms
-# of the GPLv3 license.  See the LICENSE file for details.
+# of the GPLv3 license.  See the LICENSE file for details. 
 
 import sys
 import os
