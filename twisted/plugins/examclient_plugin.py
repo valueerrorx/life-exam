2f2f40d9b7911a252849b31fb5f07674dd71ffa4
SManAT <s.hagmann@bgbaden-frauen.ac.at>
2020-11-02 17:02:29 +0100

    testing

--- twisted/plugins/examclient_plugin.py
+++ twisted/plugins/examclient_plugin.py
@@ -434,8 +434,7 @@ class MyClientProtocol(basic.LineReceiver):
             os.system(command)
             time.sleep(2)
             # start as user even if the twistd daemon is run by root
-            command = "%s/startexam.sh %s %s &" % (EXAMCONFIG_DIRECTORY, cleanup_abgabe, spellcheck)
-            print(command)  
+            command = "sudo %s/startexam.sh %s %s &" % (EXAMCONFIG_DIRECTORY, cleanup_abgabe, spellcheck)
             os.system(command)  # start script
         else:
             return  # running on the same machine.. do not start exam mode / do not copy zip content over original
